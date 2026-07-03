964c3eba5a3ccd0f0517333148c5a695cd3e4b92
dongloveyou <42081667+dongloveyou@users.noreply.github.com>
2026-07-03 14:36:25 +0700

    Add UTF-8 encoding and module path adjustment
    Ensure module path is correctly recognized across OS.

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,7 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+# Đảm bảo hệ thống nhận diện đúng đường dẫn mô-đun trên mọi hệ điều hành
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
